db85279debdcdc2249cb1e53e8f31b92482bfc91
dev-kwon <kwonoh92@snu.ac.kr>
2021-08-24 16:41:54 +0900

    result with url,title,author

--- naver-news-crawler/index.js
+++ naver-news-crawler/index.js
@@ -19,10 +19,32 @@ const getNews = () => {
                 // cheerio를 활용하여 body에서 데이터 추출
                 const bodyDecoded = iconv.decode(body, "euc-kr").toString();
                 const $ = cheerio.load(bodyDecoded);
-                const listTextInnerArr = $("#_rankingList0 > li").toArray();
+                const listTextInnerArr = $("#_rankingList0 > li > div > div > div").toArray();
                 // console.log(bodyDecoded);
                 // console.log($);
-                console.log(listTextInnerArr);
+                // console.log(listTextInnerArr);
+                const result = [];
+                listTextInnerArr.forEach((div) => {
+                    // result에 1. 뉴스글 url / 2. 뉴스제목 / 3. 작성자(언론사) 저장
+                    const aFirst = $(div).find("a").first();
+                    
+                    const path = aFirst.attr("href"); // 첫번째 a 태그
+                    const url = `https://news.naver.com/${path}` ; //도메인을 붙인 url 주소
+                    const title = aFirst.text().trim(); // 공백 제거
+
+                    // console.log(path);
+                    // console.log(title);
+                    // console.log(url, title);
+                    const aLast = $(div).find("a").last();
+                    const author = aLast.text().trim();
+
+                    result.push({
+                        url,
+                        title,
+                        author,
+                    })
+                });
+                console.log(result);
                 
             }
         }
